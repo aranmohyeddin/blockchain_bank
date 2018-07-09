e25bd1b9786b4f45494890a7f0fbf7767812b0e3
aran mohyeddin <rnmhdn@gmail.com>
2018-07-09 15:55:52 +0430

    a little modeling

--- banking/models.py
+++ banking/models.py
@@ -1,5 +1,6 @@
 from crypto.utils import sha512
 from django.db import models
+from crypto.utils import generate_rsa_keys
 
 try:
     from secrets import token_hex
@@ -10,10 +11,24 @@ except ImportError:
         return urandom(nbytes).hex()
 
 
+class Wallet():
+    wallet_id = models.CharField(max_length=20)
+    bank = models.ForeignKey(Bank, on_delete = models.CASCADE)
+    pub = models.CharField(max_length=1024)
+    pv = models.CharField(max_length=1024)
+
+
+    def __init__(self, bank):
+        self.bank = bank
+        self.pv, self.pub = generate_rsa_keys():
+        self.wallet_id = self.pub[:20]
+
+
 class Customer(models.Model):
     username = models.CharField(max_length=100)
-    password = models.CharField(max_length=1000)  # this is not the actualy password it is sha512(salt + password)
+    password = models.CharField(max_length=1000)  # this is not the actual password it is sha512(salt + password)
     salt = models.CharField(max_length=1000)
+        wallet = models.OneToOneField(Wallet)
 
     def set_password(self, password):
         self.salt = token_hex(32)
