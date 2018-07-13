44f69efe62b8bbc6eba18834c060c34c0a5fca21
Elkiscoming <Elkiscoming@gmail.com>
2018-07-14 00:29:34 +0430

    bugfix.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from typing import Dict
 import psycopg2
 import threading
 import time
+import base64
 
 import json
 import cmd, sys #cmd is used for making a repl.
@@ -124,12 +125,18 @@ class Shell_interface(cmd.Cmd):
                 }
         transactions = block.transactions
         for transaction in transactions:
+            sign = None
+            if transaction.signature and type(transaction.signature).__name__ == 'str':
+                sign = transaction.signature
+            elif transaction.signature and type(transaction.signature).__name__ == 'bytes':
+                sign = base64.b64encode(transaction.signature)
+                sign = sign.decode('utf8')
             transaction_dict = {
                     'value': float(transaction.value),
                     'id': transaction.transaction_id,
                     'sender_public_key': transaction.sender,
                     'receiver_public_key': transaction.recipient,
-                    'signature': transaction.signature,
+                    'signature': sign,
                     'input': [],
                     'output': []
                     }
@@ -308,8 +315,8 @@ class Shell_interface(cmd.Cmd):
             print('No wallet with id: {}'.format(recipient_wallet_id))
             return
         transaction = self.current_user.wallet.send_funds(recipient_public_key_str=recipient.get_keys()[0],
-                value=value,
-                blockchain=self.blockchain)
+                                                          value=value,
+                                                          blockchain=self.blockchain)
         if transaction:
             self.blockchain.append_transaction(transaction)
 
@@ -451,11 +458,16 @@ class Shell_interface(cmd.Cmd):
                 MIICdAIBADANBgkqhkiG9w0BAQEFAASCAl4wggJaAgEAAoGBAKC2mjrIV6NYytX7vd0ebjl7IlFZhs5cba13SCcGTkMc6BGnqFPkmJvvTH2wME+W05mHzhyNxqNAd/e9RjfHGMip0ZR63kCE9N/pgZJOg3uk3PbIjEDo5oblxsXtqTwR4HmjYM/o4BQgtTX5WOiBPrBLHd39zWzSOtnMasw6Vb3fAgMBAAECf12J6jpMYLWx+FyTKO6Jx52tDUxLzypMoYlU46nTAboOGQQtkMtDQY+AuARvh67LGl1BrbTwz6w02Z5Xi4brWoCCRtYoQwTXQc1VlKlagghIZp3zbl+Oj7pR0WQlUaXsrOA+pnqNJ3WysMxSiEHPg0lPHoYAfxWXSrN6DXXQMYkCQQDmnCRBmh8l59ePZiWY61N4XIE34JVcCwJCq/+1zqr6VPWMlFOo6ZYWFYLrmTBfqJwKPZvqoaRaubqbp1Trwv5DAkEAsmhjc4Nl63Zzk92UVs55SPcuhI+fi0Bl6lP4GyTMztQFFeUDoobLnGfd/AADI7Me3j8K4weN5ok17HZCRpPeNQJBAI8KrSaP/eAaRcgp+Qo4decDohdR0/Nq1LUcURmpnr52MnVHj/kHItSB9VpEBBBh2qAzhOHt769i4xAno/I1WlcCQFp3NHbOmk/bsJ+6LA4YhMfLD3uImI40CXnZOmYJMxFt0WZYyo8Paw/UW2v9VZo0qeJodUzJ99p+mSlejhzbvkECQGvLNSueACwhuxURJra3yb5mKA0K2DT9YLbC4Igv4g578/spLXZ+vCkxeRNyV5pzQ5psHzmEZ7XuoESTL1phWrY= ' +
                 key1
                 )
+
         time.sleep(1)
         self.do_login('c1 c1pass')
         self.do_get_balance(None)
+        key2 = Customer.objects.get(login__username='c2').get_keys()[0]
+        self.do_login_based_transfer('10 {}'.format(key2[:50]))
+        time.sleep(1)
+        self.do_get_balance(None)
         self.do_logout(None)
-        return self.do_quit(None)
+        self.do_quit(None)
 
 
 if __name__ == '__main__':
